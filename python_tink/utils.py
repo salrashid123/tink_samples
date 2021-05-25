eac2f5ff35e962283cb87c44510904db5ec97dbd
salrashid123 <salrashid123@gmail.com>
2021-05-24 23:17:36 -0400

    update python

--- python_tink/utils.py
+++ python_tink/utils.py
@@ -9,6 +9,7 @@ from tink import tink_config
 from tink import mac
 from tink.proto import tink_pb2
 from tink.proto import common_pb2
+from tink.integration import gcpkms
 from tink import core
 
 from tink import cleartext_keyset_handle
@@ -20,25 +21,39 @@ mac.register()
 
 class AESCipher(object):
 
-    def __init__(self, encoded_key):
+    def __init__(self, encoded_key, key_uri=None):
+      self.gcp_aead = None
+      if key_uri != None:
+        gcp_client = gcpkms.GcpKmsClient(key_uri=key_uri,credentials_path="")
+        self.gcp_aead = gcp_client.get_aead(key_uri)
+        if (encoded_key==None):
+          self.keyset_handle = tink.new_keyset_handle(aead.aead_key_templates.AES256_GCM)
+        else:
+          reader = tink.BinaryKeysetReader(base64.b64decode(encoded_key))          
+          self.keyset_handle = tink.KeysetHandle.read(reader, self.gcp_aead)
+      else:
+        if (encoded_key==None):
+          self.keyset_handle = tink.new_keyset_handle(aead.aead_key_templates.AES256_GCM)
+        else:
+          reader = tink.BinaryKeysetReader(base64.b64decode(encoded_key))
+          self.keyset_handle = cleartext_keyset_handle.read(reader)        
+
       tink_config.register()
       aead.register()
 
-      if (encoded_key==None):
-        self.keyset_handle = tink.new_keyset_handle(aead.aead_key_templates.AES256_GCM)
-      else:
-        reader = tink.BinaryKeysetReader(base64.b64decode(encoded_key))
-        self.keyset_handle = cleartext_keyset_handle.read(reader)
       self.key=self.keyset_handle.keyset_info()
       self.aead_primitive = self.keyset_handle.primitive(aead.Aead)
 
-    def printKey(self):
+    def printKeyInfo(self):
       print(self.keyset_handle.keyset_info())
 
     def getKey(self):
       iostream = io.BytesIO()
-      writer = tink.BinaryKeysetWriter(iostream)
-      writer.write(self.keyset_handle._keyset)
+      writer = tink.BinaryKeysetWriter(iostream)      
+      if self.gcp_aead != None:
+        self.keyset_handle.write(writer,self.gcp_aead)
+      else:
+        writer.write(self.keyset_handle._keyset)
       encoded_key = base64.b64encode(iostream.getvalue()).decode('utf-8')
       return base64.b64encode(iostream.getvalue()).decode('utf-8')
       
@@ -54,21 +69,36 @@ class AESCipher(object):
 
 class HMACFunctions(object):
 
-    def __init__(self, encoded_key=None):
-      if encoded_key == None:
-        self.keyset_handle = tink.new_keyset_handle(mac.mac_key_templates.HMAC_SHA256_256BITTAG)
+    def __init__(self, encoded_key, key_uri=None):
+      self.gcp_aead = None
+      if key_uri != None:
+        gcp_client = gcpkms.GcpKmsClient(key_uri=key_uri,credentials_path="")
+        self.gcp_aead = gcp_client.get_aead(key_uri)
+        if (encoded_key==None):
+          self.keyset_handle = tink.new_keyset_handle(mac.mac_key_templates.HMAC_SHA256_256BITTAG)
+        else:
+          reader = tink.BinaryKeysetReader(base64.b64decode(encoded_key))          
+          self.keyset_handle = tink.KeysetHandle.read(reader, self.gcp_aead)
       else:
-        reader = tink.BinaryKeysetReader(base64.b64decode(encoded_key.encode('utf_8')))
-        self.keyset_handle = cleartext_keyset_handle.read(reader)
+        if (encoded_key==None):
+          self.keyset_handle = tink.new_keyset_handle(mac.mac_key_templates.HMAC_SHA256_256BITTAG)
+        else:
+          reader = tink.BinaryKeysetReader(base64.b64decode(encoded_key))
+          self.keyset_handle = cleartext_keyset_handle.read(reader)        
       self.mac = self.keyset_handle.primitive(mac.Mac)
 
-    def printKey(self):
+
+
+    def printKeyInfo(self):
       print(self.keyset_handle.keyset_info())
 
     def getKey(self):
       iostream = io.BytesIO()
-      writer = tink.BinaryKeysetWriter(iostream)
-      writer.write(self.keyset_handle._keyset)
+      writer = tink.BinaryKeysetWriter(iostream)      
+      if self.gcp_aead != None:
+        self.keyset_handle.write(writer,self.gcp_aead)
+      else:
+        writer.write(self.keyset_handle._keyset)
       encoded_key = base64.b64encode(iostream.getvalue()).decode('utf-8')
       return base64.b64encode(iostream.getvalue()).decode('utf-8')
 
@@ -84,26 +114,54 @@ class HMACFunctions(object):
         return False
  
 
-c = AESCipher(encoded_key=None)
-k = c.getKey()
-c.printKey()
+
+cc = AESCipher(encoded_key=None)
+k = cc.getKey()
+cc.printKeyInfo()
 print(k)
-k = 'CNSKwpcCEmQKWAowdHlwZS5nb29nbGVhcGlzLmNvbS9nb29nbGUuY3J5cHRvLnRpbmsuQWVzR2NtS2V5EiIaIGLSL2lQeHvR5byaWT7geg8/utkX0NAvbE4b+dPx53hoGAEQARjUisKXAiAB'
-cc = AESCipher(k)
+
+cc = AESCipher(encoded_key=k)
 enc=cc.encrypt("foo".encode('utf-8'),"none")
 print(enc)
 dec = cc.decrypt(enc,"none")
 print(dec)
 
+
 h = HMACFunctions(encoded_key=None)
 k = h.getKey()
-h.printKey()
+h.printKeyInfo()
 print(k)
 
 h = HMACFunctions(encoded_key=k)
 dd = "dsfas"
 hashed=h.hash(dd.encode('utf-8'))
 print(base64.b64encode(hashed).decode('utf-8'))
-
 print(h.verify(dd.encode('utf-8'),base64.b64decode(hashed)))
 
+
+# # with KMS
+
+# keyURI="gcp-kms://projects/mineral-minutia-820/locations/us-central1/keyRings/mykeyring/cryptoKeys/key1"
+
+# cc = AESCipher(encoded_key=None,key_uri=keyURI)
+# k = cc.getKey()
+# cc.printKeyInfo()
+# print(k)
+
+# cc = AESCipher(encoded_key=k,key_uri=keyURI)
+# enc=cc.encrypt("foo".encode('utf-8'),"none")
+# print(enc)
+# dec = cc.decrypt(enc,"none")
+# print(dec)
+
+
+# h = HMACFunctions(encoded_key=None,key_uri=keyURI)
+# k = h.getKey()
+# h.printKeyInfo()
+# print(k)
+
+# h = HMACFunctions(encoded_key=k,key_uri=keyURI)
+# dd = "dsfas"
+# hashed=h.hash(dd.encode('utf-8'))
+# print(base64.b64encode(hashed).decode('utf-8'))
+# print(h.verify(dd.encode('utf-8'),base64.b64decode(hashed)))
